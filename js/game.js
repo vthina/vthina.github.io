194b00ad51419cec57d048b79ac96bae18835f61
snow <lookforalex920376@gmail.com>
2020-05-27 00:21:54 -0400

    unity upload

--- js/game.js
+++ js/game.js
@@ -2,6 +2,6 @@ $(function(){
     var url = $(location).attr('href');
     var id = Mina.parseSearch(url, "id");
 
-    $("#game").load("Game/"+id+"/index.html");
+    $("#game").load("html/Game/"+id+".html");
     console.log(id);
 });

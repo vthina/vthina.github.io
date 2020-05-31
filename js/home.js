5324b61d1870999d179aeb2f34ff67025a7936d4
snow <lookforalex920376@gmail.com>
2020-05-31 12:56:03 -0400

    web update

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 $(function(){ 
     var html_folder_path = "html/",
         label_path = html_folder_path + "label",
-        body0_path = html_folder_path + "bucket-0";
+        body0_path = html_folder_path + "news-default";
     
     $("#body0").load(body0_path + ".html");
    // $("#body1").load();
